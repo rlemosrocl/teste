e8374e9a97089caaff081ca1b6a4eda22b73c7ab
rafael <rlemosrocl@gmail.com>
2019-04-21 16:30:54 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+		<title>Hoje e <?php echo date("d/m/Y")?></title>
+	</head>
+	<body>
+<?php
+	echo "Hello, World!";
+
+	$dados = [
+		"Cachorro" => "Au au",
+		"Gato" => "Miau" 	
+	];
+
+	for ($i=0; $i<2; $i++){
+		echo "<h1>Hello " . $i . "</h1>";
+	}
+
+	foreach ($dados as $bicho => $barulho){
+		echo "<p>O " . $bicho . " faz " . $barulho . "</p>";
+	}
+?>
+	</body>
+</html>
